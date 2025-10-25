1e53c13ed9497dd7cd9093201ae3e8f79290cd1b
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-10-25 14:59:04 -0400

    attempted to make us loop the random chance multiple times

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const submitButton = document.getElementById("submitBtn");
 const answerBox = document.getElementById("Answer");
 const correctTicker = document.getElementById("Correct");
 const wrongTicker = document.getElementById("Wrong");
-
+const amountTicker = document.getElementById("diceSides");
 let Correct = 0;
 let Wrong = 0;
 
@@ -16,13 +16,14 @@ function getRandomInt(max) {
 submitButton.addEventListener("click", () => {
   const diceSides = parseInt(diceSidesElement.value);
   const userGuess = parseInt(guessElement.value);
-
+const diceAmount = parseInt(amountTicker.value);
   // Validate inputs
   if (isNaN(diceSides) || isNaN(userGuess) || diceSides < 1) {
     answerBox.textContent = "❌ Please enter valid numbers.";
     return;
   }
-
+for(let i = 0; i < diceAmount; i++)
+	{
   const rolledNumber = getRandomInt(diceSides);
 
   if (rolledNumber === userGuess) {
@@ -34,4 +35,5 @@ submitButton.addEventListener("click", () => {
     Wrong++;
     wrongTicker.textContent = Wrong;
   }
+	}
 });
