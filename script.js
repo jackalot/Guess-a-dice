b2cac5dd76a2002326c1797cc4d4bea680154837
Christopher Gonzaelz <gonzalezc9999@gmail.com>
2025-10-25 04:04:36 -0400

    we now check if the guess was right or not.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// script.js
 const diceSidesElement = document.getElementById("diceSides");
+const guessElement = document.getElementById("YourGuess");
+const submitButton = document.getElementById("submitBtn");
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+submitButton.addEventListener("click", () => {
+  const diceSides = diceSidesElement.value;
+  const userGuess = guessElement.value;
+  if (getRandomInt(diceSides) === userGuess) {
+    console.log("You win");
+  } else {
+    console.log("You Lose");
+  }
+});
